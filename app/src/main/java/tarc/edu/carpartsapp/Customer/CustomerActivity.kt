2f5f9d5c6d11a957fab4844eec5fa0a2d7f783b6
Haode012 <haodeyap0902@gmail.com>
2023-02-02 15:52:42 +0800

    Haode commit

--- app/src/main/java/tarc/edu/carpartsapp/Customer/CustomerActivity.kt
+++ app/src/main/java/tarc/edu/carpartsapp/Customer/CustomerActivity.kt
@@ -1,7 +1,10 @@
 package tarc.edu.carpartsapp.Customer
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
@@ -11,6 +14,15 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.nav_header_main.view.*
+import tarc.edu.carpartsapp.LoginFragment
 import tarc.edu.carpartsapp.R
 import tarc.edu.carpartsapp.databinding.ActivityCustomerBinding
 
@@ -18,6 +30,7 @@ class CustomerActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityCustomerBinding
+    private var firebaseAuth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,18 +47,58 @@ class CustomerActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home_customer, R.id.nav_profile_customer, R.id.nav_myCart_customer, R.id.nav_myOrder_customer, R.id.nav_aboutUs_customer, R.id.nav_logOut_customer
+                R.id.nav_home_customer, R.id.loginFragment, R.id.nav_profile_customer, R.id.nav_myCart_customer, R.id.nav_myOrder_customer, R.id.nav_aboutUs_customer, R.id.nav_logOut_customer
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+     //updateNavHeader()
     }
 
-    /*override fun onCreateOptionsMenu(menu: Menu): Boolean {
+    private fun updateNavHeader() {
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        val user = firebaseAuth.currentUser
+        val uid = user!!.uid
+
+        val ref =
+            Firebase.database("https://latestcarpartsdatabase-default-rtdb.asia-southeast1.firebasedatabase.app/")
+                .getReference("Users").child(uid)
+
+        ref.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val navView: NavigationView = binding.navView
+                val view: View = navView.getHeaderView(0)
+                val name = dataSnapshot.child("fullName").value as String?
+                val email = dataSnapshot.child("emailAddress").value as String?
+                view.textViewUsername.setText(name)
+                view.textViewEmail.setText(email)
+            }
+            override fun onCancelled(databaseError: DatabaseError) {}
+        })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.customer, menu)
         return true
-    }*/
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.action_log_out -> {
+                firebaseAuth.signOut().apply {
+                    startActivity(Intent(applicationContext, CustomerActivity::class.java))
+                    finish()
+                }
+                true
+            }
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
+        }
+    }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_customer)
