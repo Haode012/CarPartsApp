b0628687e64433a315020e11402961b84343361e
Ruthressh21 <ruthresshvikneswaran@gmail.com>
2023-02-03 19:19:12 +0800

    ruth-user profile

--- app/src/main/java/tarc/edu/carpartsapp/ProfileFragment.kt
+++ app/src/main/java/tarc/edu/carpartsapp/ProfileFragment.kt
@@ -1,24 +1,88 @@
 package tarc.edu.carpartsapp
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.UploadTask
 import tarc.edu.carpartsapp.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
-    private var binding: FragmentProfileBinding? = null
+    private var _binding: FragmentProfileBinding? = null
+    private var imageUri: Uri? = null
+    private lateinit var storage: FirebaseStorage
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentProfileBinding.inflate(inflater, container, false)
-        return binding!!.root
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // val image: ImageView = binding.profilePicture
+
+        //image.setImageResource(R.drawable.ic_baseline_person_24)
+
+        val userId = FirebaseAuth.getInstance().currentUser?.uid.toString()
+        val database =
+            Firebase.database("https://latestcarpartsdatabase-default-rtdb.asia-southeast1.firebasedatabase.app/")
+        val ref = database.getReference("Users/$userId")
+
+        ref.get().addOnSuccessListener {
+            val name = it.child("fullName").value as String
+            val email = it.child("emailAddress").value as String
+            val address = it.child("address").value as String
+            val phoneNumber = it.child("phoneNumber").value as String
+            val birthDate = it.child("birthDate").value as String
+            val gender = it.child("gender").value as String
+
+            binding.textViewLabelBigName.text = name
+            binding.textInputFullName.setText(name)
+            binding.textInputEmail.setText(email)
+            binding.textInputHomeAddress.setText(address)
+            binding.textInputPhone.setText(phoneNumber)
+            binding.textInputDateOfBirth.setText(birthDate)
+            binding.textInputGender.setText(gender)
+
+            //binding.profilePicture.setOnClickListener {
+            //  getPhoto.launch("image/*")
+            //}
+            // }
+
+              binding.buttonProfile.setOnClickListener {
+                  findNavController().navigate(R.id.action_nav_profile_customer_to_fragmentEditProfile2)
+              }
+            //   ref.child("fullName").setValue(binding.fullName.text.toString())
+            //   ref.child("emailAddress").setValue(binding.emailAddress.text.toString())
+            //   ref.child("address").setValue(binding.homeAddress.text.toString())
+            //ref.setValue(binding.emailAddress.text.toString())
+            // ref.setValue(binding.emailAddress.text.toString())
+        }
+
+    }
+
+    private fun uploadInfo() {
+    }
+
+    private fun storeData(imageUri: UploadTask.TaskSnapshot) {
+
+
+    }
+
+
 
     override fun onDestroyView() {
         super.onDestroyView()
-        binding = null
+        _binding = null
     }
 }
