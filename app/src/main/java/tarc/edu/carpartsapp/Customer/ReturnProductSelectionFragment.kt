cdb635c5951574fff2e5722a3f4def43614e0c49
Haode012 <haodeyap0902@gmail.com>
2023-02-12 15:08:52 +0800

    Haode commit

---
+++ app/src/main/java/tarc/edu/carpartsapp/Customer/ReturnProductSelectionFragment.kt
@@ -0,0 +1,28 @@
+package tarc.edu.carpartsapp.Customer
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import tarc.edu.carpartsapp.R
+import tarc.edu.carpartsapp.databinding.FragmentProfileBinding
+import tarc.edu.carpartsapp.databinding.FragmentReturnProductBinding
+
+class ReturnProductSelectionFragment : Fragment() {
+    private var _binding: FragmentReturnProductBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentReturnProductBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
